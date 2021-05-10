c0ed03330dbaa8371581b4f8d063a832f8181542
yuminko <yuminko@yonsei.ac.kr>
2021-05-10 09:07:17 +0000

    LSTMbaseline

---
+++ lib/models/lstm.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+
+from .feature_extractor import build_feature_extractor
+
+
+
+class baselineLSTM(nn.Module):
+    def __init__(self, args):
+        super(baselineLSTM, self).__init__()
+        self.hidden_size = args.hidden_size
+        self.enc_steps = args.enc_steps
+        self.num_classes = args.num_classes
+        
+        self.feature_extractor = build_feature_extractor(args)
+        self.fusion_size = self.feature_extractor.fusion_size
+        # print(self.fusion_size)
+        
+        self.lstm = nn.LSTMCell(self.fusion_size, self.hidden_size)
+        self.classifier = nn.Linear(self.hidden_size, self.num_classes)
+
+    def encoder(self, camera_input, sensor_input, enc_hx, enc_cx):
+        fusion_input = self.feature_extractor(camera_input, sensor_input)
+        enc_hx, enc_cx = self.lstm(fusion_input, (enc_hx, enc_cx))
+        enc_score = self.classifier(enc_hx)
+        return enc_hx, enc_cx, enc_score
+
+
+    def step(self, camera_input, sensor_input, enc_hx, enc_cx):
+        # Encoder -> time t
+        enc_hx, enc_cx, enc_score = \
+                self.encoder(camera_input, sensor_input, enc_hx, enc_cx)
+
+        return enc_hx, enc_cx, enc_score
+
+
+
+    def forward(self, camera_inputs, sensor_inputs):
+        batch_size = camera_inputs.shape[0]
+        enc_hx = camera_inputs.new_zeros((batch_size, self.hidden_size))
+        enc_cx = camera_inputs.new_zeros((batch_size, self.hidden_size))
+        score_stack = []
+
+        for enc_step in range(self.enc_steps):
+            enc_hx, enc_cx, enc_score = self.encoder(
+                camera_inputs[:, enc_step],
+                sensor_inputs[:, enc_step], enc_hx, enc_cx,
+            )
+            score_stack.append(enc_score)
+        
+        scores = torch.stack(score_stack, dim=1).view(-1, self.num_classes)
+
+        return scores
+
+        
---
+++ thumos_lstm_eval.py
@@ -0,0 +1,72 @@
+import os
+import os.path as osp
+import sys
+import time
+
+import torch
+import torch.nn as nn
+import numpy as np
+
+import _init_paths
+import utils as utl
+from configs.thumos import parse_lstm_args as parse_args
+from models import build_model
+
+def to_device(x, device):
+    return x.unsqueeze(0).to(device)
+
+def main(args):
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    enc_score_metrics = []
+    enc_target_metrics = []
+
+    if osp.isfile(args.checkpoint):
+        checkpoint = torch.load(args.checkpoint)
+    else:
+        raise(RuntimeError('Cannot find the checkpoint {}'.format(args.checkpoint)))
+    model = build_model(args).to(device)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    model.train(False)
+
+    softmax = nn.Softmax(dim=1).to(device)
+
+    for session_idx, session in enumerate(args.test_session_set, start=1):
+        start = time.time()
+        with torch.set_grad_enabled(False):
+            camera_inputs = np.load(osp.join(args.data_root, args.camera_feature, session+'.npy'), mmap_mode='r')
+            motion_inputs = np.load(osp.join(args.data_root, args.motion_feature, session+'.npy'), mmap_mode='r')
+            target = np.load(osp.join(args.data_root, 'target', session+'.npy'))
+            enc_hx = to_device(torch.zeros(model.hidden_size), device)
+            enc_cx = to_device(torch.zeros(model.hidden_size), device)
+
+            for l in range(target.shape[0]):
+                camera_input = to_device(
+                    torch.as_tensor(camera_inputs[l].astype(np.float32)), device)
+                motion_input = to_device(
+                    torch.as_tensor(motion_inputs[l].astype(np.float32)), device)
+
+                enc_hx, enc_cx, enc_score = \
+                        model.step(camera_input, motion_input, enc_hx, enc_cx)
+
+                enc_score_metrics.append(softmax(enc_score).cpu().numpy()[0])
+                enc_target_metrics.append(target[l])
+
+        end = time.time()
+
+        print('Processed session {}, {:2} of {}, running time {:.2f} sec'.format(
+            session, session_idx, len(args.test_session_set), end - start))
+
+    save_dir = osp.dirname(args.checkpoint)
+    result_file  = osp.basename(args.checkpoint).replace('.pth', '.json')
+    # Compute result for encoder
+
+    utl.compute_result_multilabel(args.dataset, args.class_index,
+                                  enc_score_metrics, enc_target_metrics,
+                                  save_dir, result_file, ignore_class=[0], save=True, verbose=True)
+
+
+
+if __name__ == '__main__':
+    main(parse_args())
